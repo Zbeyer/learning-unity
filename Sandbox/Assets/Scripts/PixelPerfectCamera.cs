711126d809fdb2d3bb1e4fc406090ebb6a6b3b52
Zachary Beyer <zackbeyer@live.com>
2017-01-07 12:31:28 -0800

    Added Sandbox to dump test scripts

---
+++ Sandbox/Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour {
+	
+	public static float pixelsToUnits = 1f;
+	public static float scale = 1f;
+	
+	public Vector2 nativeResolution = new Vector2 (240, 160);
+	
+	void Awake () {
+		var camera = GetComponent<Camera> ();		//Get game camera
+		
+		
+		if (camera.orthographic) {
+			//Script assumes 2D Game Type
+			scale = Screen.height/nativeResolution.y;	//get screen to resolution ratio
+			pixelsToUnits *= scale;	//Apply to PPU
+			
+			//NOTE: Unity puts the 0 position in the middle of the screen
+			camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
+		}
+	}
+	
+}
---
+++ Sandbox/Assets/Scripts/WASDMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDMovement : MonoBehaviour {
+	private static KeyCode kbPanUp = KeyCode.W;
+	private static KeyCode kbPanDown = KeyCode.S;
+	private static KeyCode kbPanLeft = KeyCode.A;
+	private static KeyCode kbPanRight = KeyCode.D;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var camera = GetComponent<Camera> ();        //Get game camera
+		var pos = camera.transform.position;
+		
+		if(Input.GetKey(kbPanUp)){
+			pos.y += 1;
+		} else if (Input.GetKey(kbPanDown)){
+			pos.y -= 1;
+		}
+		if(Input.GetKey(kbPanLeft)){
+			pos.x -= 1;
+		} else if (Input.GetKey(kbPanRight)){
+			pos.x += 1;
+		}
+		
+		camera.transform.position = pos;
+		
+	}
+}
