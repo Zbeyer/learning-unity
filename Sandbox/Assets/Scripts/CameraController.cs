bd7b7ce1b907106b6a32feec7f896b7f2fb53f52
Zbeyer <zachary@zbeyer.com>
2017-01-10 21:48:51 -0800

    Project Hierarchy cleanup

---
+++ Sandbox/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject followTarget;
+	public Vector3 targetPos;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPos = new Vector3 ( followTarget.transform.position.x, 
+								  followTarget.transform.position.y, 
+								  followTarget.transform.position.z);
+		
+		transform.position = Vector3.Lerp (	transform.position, 
+											targetPos, 
+											moveSpeed * Time.deltaTime);
+	}
+}
